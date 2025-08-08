983868fbff5a2e943e7c0189b425a6ab1545b582
ShravaniAWanjari <shravani22wanjari@gmail.com>
2025-08-08 17:48:35 +0530

    .

--- test.py
+++ test.py
@@ -1,105 +1,48 @@
-from scipy.io import loadmat
-from PIL import Image
-import numpy as np
+import torch
 import os
-from glob import glob
-import cv2
+import numpy as np
+from datasets.crowd import Crowd
+from models.vgg import vgg19
 import argparse
 
-
-def cal_new_size(im_h, im_w, min_size, max_size):
-    if im_h < im_w:
-        if im_h < min_size:
-            ratio = 1.0 * min_size / im_h
-            im_h = min_size
-            im_w = round(im_w*ratio)
-        elif im_h > max_size:
-            ratio = 1.0 * max_size / im_h
-            im_h = max_size
-            im_w = round(im_w*ratio)
-        else:
-            ratio = 1.0
-    else:
-        if im_w < min_size:
-            ratio = 1.0 * min_size / im_w
-            im_w = min_size
-            im_h = round(im_h*ratio)
-        elif im_w > max_size:
-            ratio = 1.0 * max_size / im_w
-            im_w = max_size
-            im_h = round(im_h*ratio)
-        else:
-            ratio = 1.0
-    return im_h, im_w, ratio
-
-
-def find_dis(point):
-    square = np.sum(point*points, axis=1)
-    dis = np.sqrt(np.maximum(square[:, None] - 2*np.matmul(point, point.T) + square[None, :], 0.0))
-    dis = np.mean(np.partition(dis, 3, axis=1)[:, 1:4], axis=1, keepdims=True)
-    return dis
-
-def generate_data(im_path):
-    im = Image.open(im_path)
-    im_w, im_h = im.size
-    mat_path = im_path.replace('.jpg', '_ann.mat')
-    points = loadmat(mat_path)['annPoints'].astype(np.float32)
-    idx_mask = (points[:, 0] >= 0) * (points[:, 0] <= im_w) * (points[:, 1] >= 0) * (points[:, 1] <= im_h)
-    points = points[idx_mask]
-    im_h, im_w, rr = cal_new_size(im_h, im_w, min_size, max_size)
-    im = np.array(im)
-    if rr != 1.0:
-        im = cv2.resize(np.array(im), (im_w, im_h), cv2.INTER_CUBIC)
-        points = points * rr
-    return Image.fromarray(im), points
+args = None
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Test ')
-    parser.add_argument('--origin-dir', default='/home/teddy/UCF-QNRF_ECCV18',
-                        help='original data directory')
     parser.add_argument('--data-dir', default='/home/teddy/UCF-Train-Val-Test',
-                        help='processed data directory')
+                        help='training data directory')
+    parser.add_argument('--save-dir', default='/home/teddy/vgg',
+                        help='model directory')
+    parser.add_argument('--device', default='0', help='assign device')
     args = parser.parse_args()
     return args
 
+
 if __name__ == '__main__':
     args = parse_args()
-    save_dir = args.data_dir
-    min_size = 512
-    max_size = 2048
+    os.environ['CUDA_VISIBLE_DEVICES'] = args.device.strip()  # set vis gpu
 
-    for phase in ['Train', 'Test']:
-        sub_dir = os.path.join(args.origin_dir, phase)
-        if phase == 'Train':
-            sub_phase_list = ['train', 'val']
-            for sub_phase in sub_phase_list:
-                sub_save_dir = os.path.join(save_dir, sub_phase)
-                if not os.path.exists(sub_save_dir):
-                    os.makedirs(sub_save_dir)
-                with open('{}.txt'.format(sub_phase)) as f:
-                    for i in f:
-                        im_path = os.path.join(sub_dir, i.strip())
-                        name = os.path.basename(im_path)
-                        print(name)
-                        im, points = generate_data(im_path)
-                        if sub_phase == 'train':
-                            dis = find_dis(points)
-                            points = np.concatenate((points, dis), axis=1)
-                        im_save_path = os.path.join(sub_save_dir, name)
-                        im.save(im_save_path)
-                        gd_save_path = im_save_path.replace('jpg', 'npy')
-                        np.save(gd_save_path, points)
-        else:
-            sub_save_dir = os.path.join(save_dir, 'test')
-            if not os.path.exists(sub_save_dir):
-                os.makedirs(sub_save_dir)
-            im_list = glob(os.path.join(sub_dir, '*jpg'))
-            for im_path in im_list:
-                name = os.path.basename(im_path)
-                print(name)
-                im, points = generate_data(im_path)
-                im_save_path = os.path.join(sub_save_dir, name)
-                im.save(im_save_path)
-                gd_save_path = im_save_path.replace('jpg', 'npy')
-                np.save(gd_save_path, points)
+    datasets = Crowd(os.path.join(args.data_dir, 'test'), 512, 8, is_gray=False, method='val')
+    dataloader = torch.utils.data.DataLoader(datasets, 1, shuffle=False,
+                                             num_workers=8, pin_memory=False)
+    model = vgg19()
+    device = torch.device('cuda')
+    model.to(device)
+    model.load_state_dict(torch.load(os.path.join(args.save_dir, 'best_model.pth'), device))
+    epoch_minus = []
+
+    for inputs, count, name in dataloader:
+        inputs = inputs.to(device)
+        assert inputs.size(0) == 1, 'the batch size should equal to 1'
+        with torch.set_grad_enabled(False):
+            outputs = model(inputs)
+            temp_minu = count[0].item() - torch.sum(outputs).item()
+            print(name, temp_minu, count[0].item(), torch.sum(outputs).item())
+            epoch_minus.append(temp_minu)
+
+    epoch_minus = np.array(epoch_minus)
+    mse = np.sqrt(np.mean(np.square(epoch_minus)))
+    mae = np.mean(np.abs(epoch_minus))
+    log_str = 'Final Test: mae {}, mse {}'.format(mae, mse)
+    print(log_str)
